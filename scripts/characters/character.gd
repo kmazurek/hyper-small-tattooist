b9227eafeaa84dcacfd65a1b4b7634630e8de7c6
Filip Hausman <filip.hausman@gmail.com>
2024-08-17 16:55:05 +0200

    character is drawing

---
+++ scripts/characters/character.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+@export var max_velocity: float = 200.0
+
+var is_walking: bool = false
+var current_button_index = -1
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed() && !is_walking && current_button_index < 0:
+			is_walking = true
+			if event.button_index == 2:
+				$Drawer.start_drawing(position)
+			
+			current_button_index = event.button_index
+		
+		if event.is_released() && is_walking && event.button_index == current_button_index:
+			is_walking = false
+			if event.button_index == 2:
+				$Drawer.stop_drawing(position)
+				
+			current_button_index = -1
+
+func _process(delta: float) -> void:
+	$Drawer.update_line(position)
+	
+	if is_walking:
+		var mouse_position = get_global_mouse_position()
+		
+		velocity = mouse_position - position
+		velocity = velocity.normalized() * clampf(velocity.length(), 0, max_velocity)
+		move_and_slide()
---
+++ scripts/components/drawing.gd
@@ -0,0 +1,26 @@
+class_name Drawer extends Node2D
+
+@export var color: Color
+@export var width_curve: Curve
+
+var current_line: Line2D
+
+var is_drawing: bool = false
+
+func start_drawing(current_position: Vector2) -> void:
+	is_drawing = true
+	
+	current_line = Line2D.new()
+	current_line.default_color = color
+	current_line.width_curve = width_curve
+	get_tree().root.add_child(current_line)
+	
+	current_line.add_point(current_position)
+	
+func update_line(current_position: Vector2) -> void:
+	if is_drawing:
+		current_line.add_point(current_position)
+		
+func stop_drawing(current_position: Vector2) -> void:
+	current_line.add_point(current_position)
+	is_drawing = false

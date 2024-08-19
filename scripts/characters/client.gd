8107fd68dbf3923e26e6d9345e4c7b5ada0aea81
Kuba Mazurek <zakaprov@gmail.com>
2024-08-19 21:18:30 +0200

    Play pain anim on ouch threshold exceeded

--- scripts/characters/client.gd
+++ scripts/characters/client.gd
@@ -1,33 +1,23 @@
 extends AnimatedSprite2D
 
-@onready var pain_bar = $"../../../PainBar/Bar" as ProgressBar
-
-var previous_pain = 0
+@onready var anim_player = $AnimationPlayer as AnimationPlayer
 
 func _ready() -> void:
-	play("idle")
-	
-func _process(delta: float) -> void:
-	var current_pain = pain_bar.value
-	if current_pain - previous_pain > 0:
-		play("pain")
-	elif current_pain - previous_pain <= 0:
-		play("idle")
-		
-	previous_pain = current_pain
+	anim_player.play("idle")
 
 func _on_ouch_threshold_exceeded() -> void:
 	$AudioStreamPlayer.play()
+	anim_player.play("pain")
+	anim_player.queue("idle")
 
 func on_win() -> void:
 	set_process(false)
-	play("happy")
+	anim_player.play("happy")
 	
 func on_failure() -> void:
 	set_process(false)
-	play("angry")
+	anim_player.play("angry")
 
 func reset() -> void:
 	set_process(true)
-	play("idle")
-	previous_pain = 0
+	anim_player.play("idle")

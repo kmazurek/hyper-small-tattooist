82bb2cb0f76106ba8a410ccc7c9cdf4c044eec0a
Kuba Mazurek <zakaprov@gmail.com>
2024-08-20 01:43:23 +0200

    Update title screen start button

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,10 +8,12 @@ func _enter_tree() -> void:
 	$Logo/AnimationPlayer.play("open")
 	await $Logo/AnimationPlayer.animation_finished
 	$VBoxContainer.visible = true
+	$VBoxContainer/Start/AnimatedSprite2D.play("idle")
 	$Logo/AnimationPlayer.queue("idle")
 
 func _on_start_pressed() -> void:
 	$Logo/AnimationPlayer.play("close")
+	$VBoxContainer/Start/AnimatedSprite2D.play("appear/out", -1, true)
 	await $Logo/AnimationPlayer.animation_finished
 	on_start.emit()
 

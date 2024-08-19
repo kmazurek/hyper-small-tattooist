aa7b8512b4f62a588488e3892abcec2a275b21f9
Filip Hausman <filip.hausman@gmail.com>
2024-08-19 03:34:41 +0200

    gnome animations

--- scripts/characters/character.gd
+++ scripts/characters/character.gd
@@ -7,19 +7,29 @@ var current_button_index = -1
 
 signal on_character_move(velocity: Vector2, max_velocity: float)
 
+func _ready() -> void:
+	$AnimatedSprite.connect("animation_looped", animation_looped)
+	$AnimatedSprite.play("idle")
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed() && !is_walking && current_button_index < 0:
 			is_walking = true
 			if event.button_index == 2:
+				$AnimatedSprite.play("tattooing_start")
 				$Drawer.start_drawing(position)
+			else:
+				$AnimatedSprite.play("run")
 			
 			current_button_index = event.button_index
 		
 		if event.is_released() && is_walking && event.button_index == current_button_index:
 			is_walking = false
 			if event.button_index == 2:
+				$AnimatedSprite.play("tattooing_end")
 				$Drawer.stop_drawing(position)
+			else:
+				$AnimatedSprite.play("idle")
 				
 			current_button_index = -1
 
@@ -40,12 +50,22 @@ func on_failure() -> void:
 	$Drawer.stop_drawing(position)
 	set_process(false)
 	set_process_input(false)
+	$AnimatedSprite.play("lose_start")
 	current_button_index = -1
 
 func reset() -> void:
 	is_walking = false
 	$Drawer.stop_drawing(position)
 	$Drawer.clear()
+	$AnimatedSprite.play("idle")
 	set_process(true)
 	set_process_input(true)
 	current_button_index = -1
+
+func animation_looped() -> void:
+	if $AnimatedSprite.animation == "tattooing_end":
+		$AnimatedSprite.play("idle")
+	if $AnimatedSprite.animation == "tattooing_start":
+		$AnimatedSprite.play("tattooing")
+	if $AnimatedSprite.animation == "lose_start":
+		$AnimatedSprite.play("lose_idle")

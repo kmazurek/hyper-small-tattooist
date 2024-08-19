0cb94f954d36938f3b85a6c10a0777214c5b1a2f
Kuba Mazurek <zakaprov@gmail.com>
2024-08-19 23:22:20 +0200

    Add failure screen animation

--- scripts/failure_screen.gd
+++ scripts/failure_screen.gd
@@ -2,6 +2,14 @@ extends Control
 
 signal on_restart
 
+@onready var anim_player: AnimationPlayer = $AnimatedSprite2D/AnimationPlayer
+
+func _ready() -> void:
+	$VBoxContainer.visible = false
+	anim_player.play("default")
+	await get_tree().create_timer(1.5).timeout
+	$VBoxContainer.visible = true
+
 func _on_restart_clicked() -> void:
 	on_restart.emit()
 	queue_free()

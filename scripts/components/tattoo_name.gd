643d818f5a6f19e03006ff91ba243015ac18d8c4
Kuba Mazurek <zakaprov@gmail.com>
2024-08-19 10:40:15 +0200

    Build random tattoo names

--- scripts/components/tattoo_name.gd
+++ scripts/components/tattoo_name.gd
@@ -1,14 +1,55 @@
 extends Sprite2D
 
-var tattoo_themes: Array[String] = [
-	"Feroucious duck",
-	"Brutal crab",
-	"Stinky hammer",
-	"Elephant skull"
+var adjectives1: Array[String] = [
+	"small",
+	"big",
+	"buff",
+	"long",
+	"short",
+	"giant",
+	"typical",
+	"simple",
+	"fancy"
+]
+
+var adjectives2: Array[String] = [
+	"ferocious",
+	"brutal",
+	"stinky",
+	"sexy",
+	"awesome",
+	"shy",
+	"annoying",
+	"cute",
+	"boring",
+	"weird",
+	"burning",
+	"smoking"
+]
+
+var nouns: Array[String] = [
+	"frog",
+	"orc",
+	"elf",
+	"sword",
+	"axe",
+	"skull",
+	"fox",
+	"cat",
+	"elephant",
+	"goblin",
+	"mace",
+	"star",
+	"dog",
+	"cyclops",
+	"human",
+	"dragon",
+	"rabbit",
+	"horse",
 ]
 
 func _ready() -> void:
 	reload_theme()
 
 func reload_theme() -> void:
-	$Label.text = tattoo_themes.pick_random()
+	$Label.text = "%s %s %s" % [adjectives1.pick_random(), adjectives2.pick_random(), nouns.pick_random()]

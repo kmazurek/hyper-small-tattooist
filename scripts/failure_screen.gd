ae4b0c031d473967adffa31e6806d43e83320ff7
Kuba Mazurek <zakaprov@gmail.com>
2024-08-20 11:55:21 +0200

    Add game over title animation

--- scripts/failure_screen.gd
+++ scripts/failure_screen.gd
@@ -2,14 +2,20 @@ extends Control
 
 signal on_restart
 
-@onready var anim_player: AnimationPlayer = $AnimatedSprite2D/AnimationPlayer
+@onready var jaws_anim: AnimationPlayer = $JawsSprite/AnimationPlayer
+@onready var game_over_anim: AnimationPlayer = $GameOverSprite/AnimationPlayer
 
 func _ready() -> void:
-	$VBoxContainer.visible = false
-	anim_player.play("default")
-	await get_tree().create_timer(1.5).timeout
+	jaws_anim.play("default")
+	await jaws_anim.animation_finished
+	game_over_anim.play("open")
+	$GameOverSprite.visible = true
+	await game_over_anim.animation_finished
 	$VBoxContainer.visible = true
+	game_over_anim.play("idle")
 
 func _on_restart_clicked() -> void:
+	game_over_anim.play("close")
+	await game_over_anim.animation_finished
 	on_restart.emit()
 	queue_free()
